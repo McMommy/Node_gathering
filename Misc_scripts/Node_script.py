913d1b589e04843d1b0e396b10bd13005b823793
McMommy <dylanmcquade1993@gmail.com>
2025-02-14 20:51:03 -0800

    some bugs, probably it for tonight. got a lot of work done. current node isnt changing kinda. it says its a different node so it understands, but problems like chopping copper.

--- Misc_scripts/Node_script.py
+++ Misc_scripts/Node_script.py
@@ -22,12 +22,14 @@ class Player():
 
 
 class Nodes():
-    def __init__(self, node_type, name, health, breaking_power, spawn_chance):
+    def __init__(self, node_type, name, health, max_health, breaking_power, spawn_chance, resource):
         self.node_type = node_type
         self.name = name
         self.health = health
+        self.max_health = max_health
         self.breaking_power = breaking_power
         self.spawn_chance = spawn_chance
+        self.resource = resource
 
 
 class Tools():
@@ -42,12 +44,12 @@ player1 = Player("name", 100, 100, 100)
 
 #nodes
 #ores
-iron = Nodes("ore", "iron", 60, 2, 1)
-copper = Nodes("ore", "copper", 50, 1, 1)
-stone = Nodes("ore", "stone", 40, 0, 1)
+iron = Nodes("ore", "iron", 60, 60, 2, 1, "iron_ore")
+copper = Nodes("ore", "copper", 50, 50, 1, 1, "copper_ore")
+stone = Nodes("ore", "stone", 40, 40, 0, 1, "stone")
 #wood
-oak = Nodes("tree", "oak", 50, 1, 1)
-chesnut = Nodes("tree", "chesnut", 30, 0, 1)
+oak = Nodes("tree", "oak", 50, 50, 1, 1, "oak_wood")
+chesnut = Nodes("tree", "chesnut", 30, 30, 0, 1, "chesnut_wood")
 
 #tools
 iron_pickaxe = Tools("pickaxe", "iron_pickaxe", 10, 5)
@@ -69,45 +71,52 @@ def start_game():
 
 def mine(current_tool, current_node) :
     pass
-    if current_tool.tool_type == "pickaxe" and current_tool.breaking_power >= current_node.breaking_power:
+    if current_tool.tool_type == "pickaxe" and player1.stamina > 0 and current_tool.breaking_power >= current_node.breaking_power:
         current_node.health -= current_tool.damage
-        print(f"this ore has {current_node.health} health left")
-
+        node_destroyed(current_node)
+    #elif player1.stamina <= 0:
+        #print("You're out of stamina, try taking a quick rest")
+    else:
+        print("can't do that right now")
 def chop(current_tool, current_node):
     pass
-    if current_tool.tool_type == "axe" and current_tool.breaking_power >= current_node.breaking_power:
+    if current_tool.tool_type == "axe" and player1.stamina > 0 and current_tool.breaking_power >= current_node.breaking_power:
         current_node.health -= current_tool.damage
-        print(f"this tree has {current_node.health} health left")
+        player1.stamina -= 10
+        node_destroyed(current_node)
+    #elif player1.stamina <= 0:
+        #print("You're out of stamina, try taking a quick rest")
+    else:
+        print("can't do that right now")
 
 
 def node_destroyed(current_node):
     if current_node.health <= 0:
-        current_node.health = 0
+        collect_resource(current_node)
+        current_node.health = current_node.max_health
         print(f"you broke {current_node.name} {current_node.node_type}")
-        current_node = spawn_node()
         print("your walking to your next node")
+        current_node = spawn_node()
     else:
         print(f"{current_node.name} has {current_node.health} health left")
 
 
 
 
-    current_node = spawn_node(current_node)
-    pass
-
-
-
 def collect_resource(current_node):
-    if current_node.node_type == "tree" and current_node.health <= 0:
-        pass
-    elif current_node.node_type == "ore" and current_node.health <= 0:
-        pass
+    if current_node.node_type == "ore" and current_node.health <= 0 and current_node.name in player1.inventory:
+        player1.inventory[current_node.name] += 10
+        print(f"you gained 10 {current_node.resource}")
+    elif current_node.node_type == "tree" and current_node.health <= 0 and current_node.name in player1.inventory:
+        player1.inventory[current_node.name] += 10
+    else:
+        print("go to collect_resource()")
 
 
 def spawn_node():
     farmable = [iron, copper, stone, oak, chesnut]
     current_node = random.choice(farmable)
-    print(f"you find a {current_node.node_type}")
+    print(f"you find a {current_node.name} node")
     return current_node
 
 
@@ -125,9 +134,12 @@ def text(current_tool):
         print("How did you obtain this")
 
 
+def choose_name():
+    pass
+
+
 #make it so i get food from trees, then can eat the food for stamina, return back to eat()
 def rest():
-    pass
     if player1.stamina >= 0:
         print("you sit down and take a short nap")
         gained = player1.max_stamina - player1.stamina
@@ -189,9 +201,9 @@ def work(current_tool, current_node):
             chop(current_tool, current_node)
         elif command == "rest":
             rest()
-        elif command == "inv" or command == "inventory":
+        elif command == "inv":
             access_inventory(current_tool)
-        elif command == "home" or command == "exit":
+        elif command == "home":
             print("Going Home!")
             break
         else:
